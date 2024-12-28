24e65b2883e66739c58fba287631600ca07259e3
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-28 08:39:13 +0600

    Task3

---
+++ Tyuiu.MerzhinskiyOO.Sprint4.Task3.V15.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint4.Task3.V15.Lib
+{
+    public class DataService : ISprint4Task3V15
+    {
+        public int Calculate(int[,] array)
+        {
+            int rowCount = array.GetLength(0);
+            int colCount = array.GetLength(1);
+            int oddCount = 0;
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < colCount; j++)
+                {
+                    if (array[i, j] % 2 != 0)
+                    {
+                        oddCount++;
+                    }
+                }
+            }
+
+            return oddCount;
+        }
+    }
+}
---
+++ Tyuiu.MerzhinskiyOO.Sprint4.Task3.V15/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.MerzhinskiyOO.Sprint4.Task3.V15.Lib;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint4.Task3.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на C# по заданным условиям.                          *");
+            int[,] mas2 = new int[5, 5]{    {7, 4, 2, 5, 3},
+                                            {4, 3, 2, 5, 6},
+                                            {6, 3, 4, 7, 5},
+                                            {5, 7, 4, 3, 8},
+                                            {7, 8, 8, 5, 6} };
+            int rowCount = mas2.GetLength(0);
+            int colCount = mas2.GetLength(1);
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < colCount; j++)
+                {
+                    Console.Write($"{mas2[i, j]} \t");
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(mas2);
+            Console.WriteLine("Количество нечетных элементов в массиве: " + res);
+            Console.ReadKey();
+        }
+    }
+}
