62d3aa2a402f83aa43d94b84e2fd76f55a6cc916
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-28 08:28:34 +0600

    Task1

---
+++ Tyuiu.MerzhinskiyOO.Sprint4.Task1.V16/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.MerzhinskiyOO.Sprint4.Task1.V16.Lib;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint4.Task1.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int len;
+            Console.WriteLine("Введите количество элементов массива: ");
+            len = Convert.ToInt32(Console.ReadLine());
+
+            int[] numsArray = new int[len];
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.Write("Введите значение " + i + " элемента массива: ");
+                numsArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.Write(numsArray[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(numsArray);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
